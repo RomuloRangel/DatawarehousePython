f1f3676336df221574833e55fd6a31e03fc8ad51
Romulo Rangel <romulordocumentos@gmail.com>
2024-07-06 17:28:45 -0300

    Configuração banco de dados postgres usando render

--- src/extract_load.py
+++ src/extract_load.py
@@ -8,24 +8,25 @@ import os
 # Import das variaveis de ambiente
 
 commodities = ['CL=F', 'GC=F', 'SI=f']
-def buscar_dados(simbolo, periodo= '5d' , intervalo= '1d'):
-    
-    ticker = yf.Ticker('')
+def buscar_dados_commodities(simbolo, periodo='5d', intervalo='1d'):
+    ticker = yf.Ticker(simbolo)
     dados = ticker.history(period=periodo, interval=intervalo)[['Close']]
     dados['simbolo'] = simbolo
     return dados
 
-def buscar_todos_dados(commodities):
+def buscar_todos_dados_commodities(commodities):
     todos_dados = []
     for simbolo in commodities:
-        dados = buscar_todos_dados(simbolo)
+        dados = buscar_dados_commodities(simbolo)
         todos_dados.append(dados)
     return pd.concat(todos_dados)
 
-if __name__ == "__main__":
-    dados_concatenados = buscar_todos_dados(commodities)
-    print(dados_concatenados)
 
+
+if __name__ == "__main__":
+    dados_concatenados = buscar_todos_dados_commodities(commodities)
+    print(dados_concatenados)
+    
 # pegar cotação dos meus ativos
 
 # concatenar ativos (1..2..3) -> (1) 
