42a0b8e253a983e8e662c5e577ddd04348078542
Romulo Rangel <romulordocumentos@gmail.com>
2024-07-06 18:07:56 -0300

    Commit envio para Database

--- src/extract_load.py
+++ src/extract_load.py
@@ -8,6 +8,19 @@ import os
 # Import das variaveis de ambiente
 
 commodities = ['CL=F', 'GC=F', 'SI=f']
+
+DB_HOST= os.getenv('DB_HOST_PROD')
+DB_PORT= os.getenv('DB_PORT_PROD')
+DB_NAME= os.getenv('DB_NAME_PROD')
+DB_USER= os.getenv('DB_USER_PROD')
+DB_PASS= os.getenv('DB_PASS_PROD')
+DB_SCHEMA= os.getenv('DB_SCHEMA_PROD')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
+
 def buscar_dados_commodities(simbolo, periodo='5d', intervalo='1d'):
     ticker = yf.Ticker(simbolo)
     dados = ticker.history(period=periodo, interval=intervalo)[['Close']]
@@ -21,14 +34,12 @@ def buscar_todos_dados_commodities(commodities):
         todos_dados.append(dados)
     return pd.concat(todos_dados)
 
+def salvar_no_postgres(df, schema='public'):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
 
 
 if __name__ == "__main__":
     dados_concatenados = buscar_todos_dados_commodities(commodities)
     print(dados_concatenados)
+    salvar_no_postgres(dados_concatenados, schema='public')
     
-# pegar cotação dos meus ativos
-
-# concatenar ativos (1..2..3) -> (1) 
-
-# Salvar banco de dados
